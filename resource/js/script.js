9e5728404b92ce42a5fec5753c6c775feef28e11
loicbtd <loic.bert.marcel@gmail.com>
2019-09-30 00:12:33 -0400

    parallax test

--- resource/js/script.js
+++ resource/js/script.js
@@ -19,3 +19,24 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
